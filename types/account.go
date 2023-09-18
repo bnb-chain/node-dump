ea39f2e72a9f8a15b4a8397ce67e43ba503ca0f8
j75689 <j75689@gmail.com>
2023-09-18 18:00:15 +0800

    fix: node hash generation

--- types/account.go
+++ types/account.go
@@ -21,17 +21,24 @@ type ExportedAccount struct {
 // Serialize implements merkle tree data Serialize method.
 func (acc *ExportedAccount) Serialize() ([]byte, error) {
 	coinBytes := bytes.NewBuffer(nil)
-	for _, coin := range acc.SummaryCoins {
-		var b [32]byte
-		copy(b[:], coin.Denom)
-		coinBytes.Write(b[:])
-		coinBytes.Write(big.NewInt(coin.Amount).Bytes())
+	for index, coin := range acc.SummaryCoins {
+		coinBytes.Write(tokenHash(int64(index), coin.Denom, coin.Amount))
 	}
 	return crypto.Keccak256Hash(
 		acc.Address.Bytes(),
 		big.NewInt(acc.AccountNumber).Bytes(),
 		coinBytes.Bytes(),
 	).Bytes(), nil
+}
+
+func tokenHash(index int64, denom string, amount int64) []byte {
+	var symbol [32]byte
+	copy(symbol[:], denom)
+
+	return crypto.Keccak256Hash(
+		big.NewInt(index).Bytes(),
+		symbol[:],
+		big.NewInt(amount).Bytes()).Bytes()
 }
 
 // ExportedAssets is a map of asset name to amount
