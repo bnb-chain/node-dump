6b41427f9df1459b644d9f332a4d7b677dc02d82
dylanhuang <j75689@gmail.com>
2024-01-08 19:40:49 +0800

    feat: implement dump tool for bc-fusion (#2)
    * feat: add block_height, commitID, chainID to exported state
    
    * fix: oom issue
    
    * fix: use Keccak256 hash function
    
    * feat: add trace log
    
    * fix: remove default NumRoutines
    
    * fix: replace json pkg by "github.com/json-iterator/go"
    
    * feat: export frozen, locked coins
    
    * fix: oom issue
    
    * feat: add asset owner
    
    * docs: update readme
    
    * fix: leaf node hash function
    
    * fix: change output
    
    * fix: node hash generation
    
    * refactor: change proof generator
    
    * fix: fixed length of proofs raw data
    
    * fix: remove token index
    
    * fix: change account model field
    
    * add error checks and simplify codes
    
    * fix: wording
    
    * fix: clean codes
    
    * fix: dump all account token and proof
    
    ---------
    
    Co-authored-by: forcodedancing <just.haha.it@gmail.com>

--- cmd/dump/main.go
+++ cmd/dump/main.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/big"
 	"os"
 	"path"
 	"path/filepath"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -21,7 +24,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/bnb-chain/node/app"
-	"github.com/bnb-chain/node/wire"
+	nodetypes "github.com/bnb-chain/node/common/types"
 
 	mt "github.com/txaty/go-merkletree"
 
@@ -32,75 +35,212 @@ const (
 	flagTraceStore = "trace-store"
 )
 
-// ExportAccounts exports blockchain world state to json.
-func ExportAccounts(app *app.BNBBeaconChain) (appState json.RawMessage, err error) {
+func NewHashFunc(data []byte) ([]byte, error) {
+	return crypto.Keccak256(data), nil
+}
+
+type leafNode struct {
+	Address sdk.AccAddress `json:"address"`
+	Coin    sdk.Coin       `json:"coin"`
+}
+
+// Serialize implements merkle tree data Serialize method.
+func (node *leafNode) Serialize() ([]byte, error) {
+	var symbol [32]byte
+	copy(symbol[:], node.Coin.Denom)
+	return crypto.Keccak256(
+		node.Address.Bytes(),
+		symbol[:],
+		big.NewInt(node.Coin.Amount).FillBytes(make([]byte, 32)),
+	), nil
+}
+
+func (node *leafNode) Print() string {
+	buf, _ := node.Serialize()
+
+	return "0x" + common.Bytes2Hex(crypto.Keccak256(buf))
+}
+
+// ExportAccountsBalanceWithProof exports blockchain world state to json.
+func ExportAccountsBalanceWithProof(app *app.BNBBeaconChain, outputPath string) (err error) {
 	ctx := app.NewContext(sdk.RunTxModeCheck, abci.Header{})
 
 	// iterate to get the accounts
 	accounts := []*types.ExportedAccount{}
 	mtData := []mt.DataBlock{}
-	assets := types.ExportedAssets{}
+
 	appendAccount := func(acc sdk.Account) (stop bool) {
-		addr := acc.GetAddress()
-		coins := acc.GetCoins()
-		for _, coin := range coins {
-			assets[coin.Denom] += coins.AmountOf(coin.Denom)
-		}
+		namedAcc := acc.(nodetypes.NamedAccount)
+		addr := namedAcc.GetAddress()
+		coins := namedAcc.GetCoins()
+		frozenCoins := namedAcc.GetFrozenCoins()
+		lockedCoins := namedAcc.GetLockedCoins()
+
+		allCoins := coins.Plus(frozenCoins)
+		allCoins = allCoins.Plus(lockedCoins)
+
 		account := types.ExportedAccount{
 			Address:       addr,
-			AccountNumber: acc.GetAccountNumber(),
-			Coins:         acc.GetCoins(),
+			AccountNumber: namedAcc.GetAccountNumber(),
+			Coins:         allCoins.Sort(),
 		}
 		accounts = append(accounts, &account)
-		mtData = append(mtData, &account)
 
-		if err != nil {
-			fmt.Println(err)
-			return true
+		for index := range allCoins {
+			if allCoins[index].Amount > 0 {
+				mtData = append(mtData, &leafNode{
+					Address: addr,
+					Coin:    allCoins[index],
+				})
+			}
 		}
+
+		trace("address", acc.GetAddress(), "account:", account)
 
 		return false
 	}
 
+	trace("iterate accounts...")
 	app.AccountKeeper.IterateAccounts(ctx, appendAccount)
+
+	trace("make merkle tree...")
 	// create a Merkle Tree config and set parallel run parameters
 	config := &mt.Config{
-		RunInParallel:    true,
-		NumRoutines:      4,
-		SortSiblingPairs: true,
+		HashFunc:           NewHashFunc,
+		RunInParallel:      true,
+		SortSiblingPairs:   true,
+		DisableLeafHashing: true,
 	}
 
 	tree, err := mt.New(config, mtData)
 	if err != nil {
-		return nil, err
+		return err
 	}
+
+	trace("make proofs...")
 	proofs := tree.Proofs
-	exportedProof := make(types.ExportedProofs, len(proofs))
+	maxProofLength := 0
+	exportedProof := make([]*types.ExportedProof, 0, len(proofs))
+	trace("proofs length", len(proofs))
 	for i := 0; i < len(mtData); i++ {
 		proof := proofs[i]
 		nProof := make([]string, 0, len(proof.Siblings))
-		for i := 0; i < len(proof.Siblings); i++ {
-			nProof = append(nProof, "0x"+common.Bytes2Hex(proof.Siblings[i]))
+		for j := 0; j < len(proof.Siblings); j++ {
+			nProof = append(nProof, "0x"+common.Bytes2Hex(proof.Siblings[j]))
 		}
-		exportedProof[accounts[i].Address.String()] = nProof
+
+		leaf := mtData[i].(*leafNode)
+		exportedProof = append(exportedProof, &types.ExportedProof{
+			Address: leaf.Address,
+			Coin:    leaf.Coin,
+			Proof:   nProof,
+		})
+		if proofLength := len(proof.Siblings); proofLength > maxProofLength {
+			maxProofLength = proofLength
+		}
+		trace("address:", accounts[i].Address.String(), "proof:", nProof, "leaf:", leaf.Print())
 	}
+	trace("max proof length:", maxProofLength)
+
 	genState := types.ExportedAccountState{
-		Accounts:  accounts,
-		Assets:    assets,
-		StateRoot: "0x" + common.Bytes2Hex(tree.Root),
-		Proofs:    exportedProof,
+		ChainID:     app.CheckState.Ctx.ChainID(),
+		BlockHeight: app.LastBlockHeight(),
+		CommitID:    app.LastCommitID(),
+		Accounts:    accounts,
+		StateRoot:   "0x" + common.Bytes2Hex(tree.Root),
+		Proofs:      exportedProof,
 	}
-	appState, err = wire.MarshalJSONIndent(app.Codec, genState)
+
+	trace("write to file...")
+
+	// write the state to the file
+	baseFile, err := os.OpenFile(path.Join(outputPath, "base.json"), os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return appState, nil
+	defer baseFile.Close()
+	err = writeJSONFile(baseFile, genState)
+	if err != nil {
+		return err
+	}
+
+	// write the accounts to the file
+	accountFile, err := os.OpenFile(path.Join(outputPath, "accounts.json"), os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer accountFile.Close()
+	err = writeJSONFileInStream(accountFile, func(encoder *json.Encoder) error {
+		for i, account := range genState.Accounts {
+			err = encoder.Encode(account)
+			if err != nil {
+				return err
+			}
+			if i < len(accounts)-1 {
+				_, err = accountFile.WriteString(`,`)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// write the proofs to the file
+	proofFile, err := os.OpenFile(path.Join(outputPath, "proofs.json"), os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer proofFile.Close()
+	err = writeJSONFileInStream(proofFile, func(encoder *json.Encoder) error {
+		for i, proof := range genState.Proofs {
+			err = encoder.Encode(proof)
+			if err != nil {
+				return err
+			}
+			if i < len(proofs)-1 {
+				_, err = proofFile.WriteString(`,`)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeJSONFile(file *os.File, data interface{}) error {
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	return encoder.Encode(data)
+}
+func writeJSONFileInStream(file *os.File, marshal func(*json.Encoder) error) error {
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	if _, err := file.WriteString(`[`); err != nil {
+		return err
+	}
+	if err := marshal(encoder); err != nil {
+		return err
+	}
+	if _, err := file.WriteString(`]`); err != nil {
+		return err
+	}
+	return nil
 }
 
 // ExportCmd dumps app state to JSON.
 func ExportCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "export <path/state.json>",
+		Use:   "export <path>",
 		Short: "Export state to JSON",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) != 1 {
@@ -137,24 +277,11 @@ func ExportCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 			}
 
 			dapp := app.NewBNBBeaconChain(ctx.Logger, db, traceWriter)
-			output, err := ExportAccounts(dapp)
+			err = ExportAccountsBalanceWithProof(dapp, args[0])
 			if err != nil {
 				return err
 			}
 
-			outputPath := args[0]
-			file, err := os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-
-			encoder := json.NewEncoder(file)
-			encoder.SetIndent("", "\t")
-			err = encoder.Encode(output)
-			if err != nil {
-				return err
-			}
 			return nil
 		},
 	}
@@ -188,6 +315,18 @@ func openTraceWriter(traceWriterFile string) (w io.Writer, err error) {
 	return
 }
 
+func trace(a ...any) {
+	if traceLog {
+		a = append([]any{"time:", time.Now()}, a...)
+		fmt.Println(a...)
+	}
+}
+
+var (
+	// TraceLog is a flag to print out full stack trace on errors
+	traceLog = false
+)
+
 func main() {
 	cdc := app.Codec
 	ctx := app.ServerContext
@@ -199,7 +338,7 @@ func main() {
 	}
 
 	rootCmd.AddCommand(ExportCmd(ctx.ToCosmosServerCtx(), cdc))
-
+	rootCmd.PersistentFlags().BoolVar(&traceLog, "tracelog", false, "print out full stack trace on errors")
 	// prepare and add flags
 	executor := cli.PrepareBaseCmd(rootCmd, "BC", app.DefaultNodeHome)
 	err := executor.Execute()
